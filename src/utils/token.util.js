5a818fe4a06d9b2f36e04ef29c4b86f8959cacfc
verastian <verastian0908@gmail.com>
2022-09-22 20:38:40 -0300

    feature: utilities for token are created

---
+++ src/utils/token.util.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+
+const createToken = (payload, expires) => {
+
+    const token = jwt.sign(payload, process.env.SECRET, {
+        expiresIn: expires,
+    });
+
+    return token;
+};
+
+const decodeToken = (token) => {
+
+    const { id } = jwt.verify(token, process.env.SECRET);
+    return id
+};
+
+module.exports = { createToken, decodeToken };
