5f453040ebf9decc6f3f2ae06b23bb68f92475dd
Juan-Esteban-Echeverria <jeecheverria@uc.cl>
2022-10-04 16:04:19 -0300

    implementacion del logout

--- src/controllers/logout.controller.js
+++ src/controllers/logout.controller.js
@@ -1,7 +1,7 @@
 const httpStatus = require("http-status");
 
 const logout = (req, res) => {
-
+    
     try {
             localStorage.removeItem("token")
             res.status(httpStatus.OK).json({ success: true, message: "Logout Success"});
