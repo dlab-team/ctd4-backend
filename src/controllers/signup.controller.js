a86f5471a45208482e94d0bf393001d2f5c79df9
Daniela Hernández <daniebeth.dev@gmail.com>
2022-10-04 22:04:37 -0300

    arreglamos respuestas

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -9,34 +9,30 @@ const userSignup = async (req, res, next) => {
     
     // Validacion campos vacios
     if(!email || !password || !re_password){
-      res.status(httpStatus.BAD_REQUEST);
-      return res.json({ message: 'Algunos campos estan vacios'});
+      return res.status(httpStatus.BAD_REQUEST).json({ message: 'Algunos campos estan vacios'});
     }
     
     // Validacion contraseñas desiguales
     if(password != re_password){
-      res.status(httpStatus.BAD_REQUEST);
-      return res.json({ message: 'Contraseñas desiguales'});
+      return res.status(httpStatus.BAD_REQUEST).json({ message: 'Contraseñas desiguales'});
     }
     
 		// Validacion requisitos contraseña
     const regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
 		if (regex.test(password) === false) {
-      res.status(httpStatus.BAD_REQUEST);
-      return res.json({ message: 'La contraseña no cumple con los requisitos'});
+      return res.status(httpStatus.BAD_REQUEST).json({ message: 'La contraseña no cumple con los requisitos'});
 		}
     
     //Validación dentro de la base de datos
     const existingUser = await Users.findOne({ where: { email: email } })
     if (existingUser) {
-      res.status(httpStatus.BAD_REQUEST);
-      return res.json({ message: 'Este correo ya se encuentra registrado'});
+      return res.status(httpStatus.BAD_REQUEST).json({ message: 'Este correo ya se encuentra registrado'});
     }
     
     // Encriptacion de la contraseña
     const passwordHash = await passwordHashing(password)
     const user = await new Users({ email, password: passwordHash })
-    const savedUser = await user.save()
+    await user.save()
     return res.status(httpStatus.OK).json({
       message: 'Usuario autenticado exitosamente',
       user: {email}
