3d92f884b3175b0cfdcc729cf3df7bad766d65bc
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-10-11 19:41:47 -0300

    feat: creacion de migraciones para country, city y user

--- src/models/city.js
+++ src/models/city.js
@@ -1,34 +1,30 @@
 'use strict'
 const { Model } = require('sequelize')
-
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class City extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      this.belongsTo(models.City, {
+      this.belongsTo(models.Country, {
+        foreignKey: 'countryId'
+      })
+      this.hasMany(models.User, {
         foreignKey: 'cityId'
       })
     }
   }
-  User.init(
+  City.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
       name: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      phoneNumber: DataTypes.STRING,
-      address: DataTypes.STRING,
-      status: DataTypes.ENUM('pending', 'active', 'jobReady')
+      countryId: DataTypes.INTEGER
     },
     {
       sequelize,
-      modelName: 'Users'
+      modelName: 'City'
     }
   )
-  return User
+  return City
 }
