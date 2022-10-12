a03a957cc9a7ce05cd8118b1721311b6c6f0da0a
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-10-11 23:50:41 -0300

    hf User model

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -1,43 +1,50 @@
-const httpStatus = require('http-status');
-const { json } = require('sequelize');
-const { Users } = require('../models');
-const { passwordHashing } = require('../utils/password.util');
+const httpStatus = require('http-status')
+const { json } = require('sequelize')
+const { User } = require('../models')
+const { passwordHashing } = require('../utils/password.util')
 
 const userSignup = async (req, res, next) => {
   try {
     const { email, password, re_password } = req.body
-    
+
     // Validacion campos vacios
-    if(!email || !password || !re_password){
-      return res.status(httpStatus.BAD_REQUEST).json({ message: 'Algunos campos estan vacios'});
+    if (!email || !password || !re_password) {
+      return res
+        .status(httpStatus.BAD_REQUEST)
+        .json({ message: 'Algunos campos estan vacios' })
     }
-    
+
     // Validacion contraseñas desiguales
-    if(password != re_password){
-      return res.status(httpStatus.BAD_REQUEST).json({ message: 'Contraseñas desiguales'});
+    if (password != re_password) {
+      return res
+        .status(httpStatus.BAD_REQUEST)
+        .json({ message: 'Contraseñas desiguales' })
     }
-    
-		// Validacion requisitos contraseña
-    const regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
-		if (regex.test(password) === false) {
-      return res.status(httpStatus.BAD_REQUEST).json({ message: 'La contraseña no cumple con los requisitos'});
-		}
-    
+
+    // Validacion requisitos contraseña
+    const regex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})')
+    if (regex.test(password) === false) {
+      return res
+        .status(httpStatus.BAD_REQUEST)
+        .json({ message: 'La contraseña no cumple con los requisitos' })
+    }
+
     //Validación dentro de la base de datos
-    const existingUser = await Users.findOne({ where: { email: email } })
+    const existingUser = await User.findOne({ where: { email: email } })
     if (existingUser) {
-      return res.status(httpStatus.BAD_REQUEST).json({ message: 'Este correo ya se encuentra registrado'});
+      return res
+        .status(httpStatus.BAD_REQUEST)
+        .json({ message: 'Este correo ya se encuentra registrado' })
     }
-    
+
     // Encriptacion de la contraseña
     const passwordHash = await passwordHashing(password)
-    const user = await new Users({ email, password: passwordHash })
+    const user = await new User({ email, password: passwordHash })
     await user.save()
     return res.status(httpStatus.OK).json({
       message: 'Usuario autenticado exitosamente',
-      user: {email}
+      user: { email }
     })
-
   } catch (error) {
     return next(error)
   }
--- src/models/user.js
+++ src/models/user.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'Users'
+      modelName: 'User'
     }
   )
   return User
