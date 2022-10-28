4fb2027133ee593829e8a7658757a652989c6a29
Emerson Espinoza <xinayespinoza@gmail.com>
2022-10-28 18:27:56 -0300

    fix user.service

--- src/services/city.service.js
+++ src/services/city.service.js
@@ -2,7 +2,7 @@ const { City } = require('../models')
 
 const getCity = async (name) => {
   console.log('Servicios ', name)
-  const city = await City.findOne({ where: { name } })
+  const city = await City.findOne({ where: { name: name } })
   return city
 }
 const getCities = () => {}
