a3dbde6900af12f55bcd757b53c9c25e4778e0ea
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-11-04 21:05:05 -0300

    hf: category include skills

--- src/models/category.js
+++ src/models/category.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     /**
@@ -10,15 +8,19 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      this.hasMany(models.Skill)
+      this.hasMany(models.Skill, {
+        foreignKey: 'categoryId'
+      })
     }
   }
-  Category.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Category',
-  });
-  return Category;
-};
+  Category.init(
+    {
+      name: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Category'
+    }
+  )
+  return Category
+}
