a779c8592ac3e1c387409b191fe14ac2da7405c0
verastian <verastian0908@gmail.com>
2022-11-10 19:57:25 -0300

    the "Nodemailer" dependency was installed, the corresponding environment variables were created, a utility for emails and the methods to recover, verify and create a new password were created :wq

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,10 @@
-const service = require('../services')
+const { userService } = require('../services')
 const httpStatus = require('http-status')
 const { User } = require('../models')
 const { passwordHashing } = require('../utils/password.util')
+const { emailForgotPassword } = require('../utils/emails.util')
+const { createToken, decodeToken } = require('../utils/token.util')
 
-const userService = service.userService
 
 const userAuth = async (req, res, next) => {
   try {
@@ -80,5 +81,88 @@ const userLogout = (req, res) => {
     throw Error(error)
   }
 }
+const recoveryPassword = async (req, res) => {
 
-module.exports = { userAuth, userSignup, userLogout }
+  try {
+    const { email } = req.body
+
+    const user = await userService.getUserByEmail(email)
+
+    if (!user) {
+      const error = new Error('User not found!')
+      return res.status(httpStatus.NOT_FOUND).json({ msg: error.message })
+    }
+    const expiresIn = 60 * 60;
+
+    user.token = createToken({ id: user._id, email: user.email }, expiresIn);
+
+    await userService.saveUser(user)
+
+    // send email
+    const { email: _email, name, token } = user;
+
+    emailForgotPassword({ _email, name, token })
+    console.log(_email)
+
+    res.status(httpStatus.OK).json({ msg: "Se ha enviado un correo electrónico de confirmación a su correo electrónico." });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      msg: error.mesagge
+    })
+  }
+}
+const verifyPassword = async (req, res) => {
+  try {
+    const { token } = req.params
+    const email = decodeToken(token)
+    const user = await userService.getUserByEmail(email)
+
+    if (!token) {
+      const error = new Error('No tienes los permisos necesarios')
+      res.status(httpStatus.FORBIDDEN).json({ msg: '', errors: error.message });
+    }
+    if (!user) {
+      const error = new Error('Usuario no Encontrado!')
+      return res.status(httpStatus.NOT_FOUND).json({ msg: error.message })
+    }
+
+    res.status(httpStatus.OK).json({ msg: "la contraseña ha sido verificada" });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      msg: error.mesagge
+    })
+  }
+}
+const createPassword = async (req, res) => {
+  try {
+    const { token } = req.params
+    const { password } = req.body
+    const email = decodeToken(token)
+    const user = await userService.getUserByEmail(email)
+
+    if (!token) {
+      const error = new Error('No tienes los permisos necesarios')
+      res.status(httpStatus.FORBIDDEN).json({ msg: '', errors: error.message });
+    }
+    if (!user) {
+      const error = new Error('Usuario no Encontrado!')
+      return res.status(httpStatus.NOT_FOUND).json({ msg: error.message })
+    }
+    // TODO: define or not a new password validation method
+    user.password = password;
+    user.token = null;
+
+    await userService.saveUser(user)
+
+    res.status(httpStatus.OK).json({ msg: "Se ha creado una nueva contraseña con éxito", data: user });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      msg: error.mesagge
+    })
+  }
+}
+
+module.exports = { userAuth, userSignup, userLogout, recoveryPassword, createPassword, verifyPassword }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,5 +5,8 @@ const { authController } = require('../controllers')
 router.post('/login', authController.userAuth)
 router.post('/signup', authController.userSignup)
 router.post('/logout', authController.userLogout)
-
+router.post("/recovery-password", authController.recoveryPassword);
+router.route("/new-password/:token")
+    .get(authController.verifyPassword)
+    .post(authController.createPassword)
 module.exports = router
