29fb4cccec0c27863f242a3a9b4cc686f30a3f4f
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-11-19 03:13:59 -0300

    change way to lauch and deploy backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
   development: {
@@ -6,14 +6,16 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
-    dialect: "postgres"
+    port: process.env.POSTGRES_PORT,
+    dialect: 'postgres'
   },
   production: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
+    port: process.env.POSTGRES_PORT,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
