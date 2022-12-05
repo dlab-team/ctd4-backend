e2f0119f56961266a87b2bbc1ab9bc9546588de3
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-12-05 02:45:44 -0300

    add swagger and delete method for labels

--- src/controllers/label.controller.js
+++ src/controllers/label.controller.js
@@ -1,47 +1,45 @@
-const { Router } = require('express');
-const httpStatus = require('http-status');
-const { json } = require('sequelize');
+const { Router } = require('express')
+const httpStatus = require('http-status')
+const { json } = require('sequelize')
 const { labelService } = require('../services')
-const { Labels } = require('../models');
+const { Labels } = require('../models')
 
-const getLabel = async (req, res) => { 
-    try {
-        const labels = await Labels.findAll({
-            attributes: [ 'name']
-        })
-        res.json(labels)
-    } catch (error) {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            ok: false,
-            message: console.error.message
-        })
-    }
-};
+const getLabel = async (req, res) => {
+  try {
+    const labels = await Labels.findAll({
+      attributes: ['name']
+    })
+    res.json(labels)
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      message: console.error.message
+    })
+  }
+}
 
 const postLabel = async (req, res) => {
-    try { 
-   
-        const { name } = req.body
-        const label = await new Labels( { name } )
-        const result = await labelService.saveLabel(label)
-        res.json(result)
-
-    } catch (error) {
-       res.status(500).send("algo salio mal...")
-    }
+  try {
+    const { name } = req.body
+    const label = await new Labels({ name })
+    const result = await labelService.saveLabel(label)
+    res.json(result)
+  } catch (error) {
+    res.status(500).send('algo salio mal...')
+  }
 }
 
+const deleteLabel = async (req, res) => {
+  try {
+    const { id } = req.params
+    const result = await labelService.deleteLabel(id)
 
-const deleteLabel = async ( req, res ) => {
-    try {
-        const { name } = req.query;
-
-        const result = await deleteLabel( { name } );
-
-        res.json(result)
-    } catch (error) {
-        res.status(500).send("algo salio mal...")
-    }
+    return res.status(httpStatus.OK).json({
+      msg: 'La etiqueta se elimino correctamente'
+    })
+  } catch (error) {
+    res.status(500).send('algo salio mal...')
+  }
 }
 
-module.exports = { getLabel, postLabel, deleteLabel } 
+module.exports = { getLabel, postLabel, deleteLabel }
--- src/services/label.service.js
+++ src/services/label.service.js
@@ -1,33 +1,37 @@
-const httpStatus = require('http-status');
-
-
+const httpStatus = require('http-status')
+const { Labels } = require('../models')
 
 const saveLabel = async (label) => {
-    try {
-        const newLabel = await label.save()
-        return newLabel
-    } catch (error) {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            ok: false,
-            msg: error.mesagge
-        });
-    }
-};
+  try {
+    const newLabel = await label.save()
+    return newLabel
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      msg: error.mesagge
+    })
+  }
+}
 
+const deleteLabel = async (id) => {
+  try {
+    const label = await Labels.destroy({
+      where: {
+        id
+      }
+    })
 
-const deleteLabel = async (name) => {
-     try {
-        const result = await name.delete()
-        return result
-    } catch (error) {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            ok: false,
-            msg: error.mesagge
-        });
-    }
-};
+    return label
+  } catch (error) {
+    console.log('===>', error)
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      msg: error.mesagge
+    })
+  }
+}
 
 module.exports = {
-   saveLabel,
-   deleteLabel
+  saveLabel,
+  deleteLabel
 }
