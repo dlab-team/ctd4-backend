5996ad828b153f77673c2bb13549a0b540ae49cd
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-12-06 09:56:36 -0300

    update config deploy

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 const cookieSession = require('cookie-session')
-const routes = require("./src/routes/");
+const routes = require('./src/routes/')
 
-const app = express();
+const app = express()
 
-const jwt = require("jsonwebtoken")
-
+const jwt = require('jsonwebtoken')
 
 // cors config
 var corsOptions = {
@@ -17,41 +16,41 @@ var corsOptions = {
 }
 
 // middleware para las cookies
-app.use(cookieSession({
-  name: 'session',
-  keys: 'clave',
-  // opciones de las cookies
-  maxAge: 24 * 60 * 60 * 1000 // duracion de 24 horas
-}))
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: 'clave',
+    // opciones de las cookies
+    maxAge: 24 * 60 * 60 * 1000 // duracion de 24 horas
+  })
+)
 
-
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 // middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use("/", routes);
+app.use('/', routes)
 
-app.get("/", function (_req, res) {
+app.get('/', function (_req, res) {
   res.send({
-    name: "Devsafio API",
-    environment: process.env.NODE_ENV,
-  });
-});
+    name: 'Devsafio API',
+    environment: process.env.NODE_ENV
+  })
+})
 
-app.get("/ping", function (_req, res) {
-  res.send("pong");
-});
-
+app.get('/ping', function (_req, res) {
+  res.send('pong')
+})
 
 /* istanbul ignore if */
 //*This means: Run app.listen(8080) only if you are running the file
 if (!module.parent) {
   const server = app.listen(process.env.PORT, function () {
-    const port = server.address().port;
-    console.log("Example app listening at http://localhost:%s", port);
-  });
+    const port = server.address().port
+    console.log('Example app listening at http://localhost:%s', port)
+  })
 }
 
-module.exports = app;
+module.exports = app
--- src/config/config.js
+++ src/config/config.js
@@ -9,19 +9,8 @@ module.exports = {
     port: process.env.POSTGRES_PORT,
     dialect: 'postgres'
   },
-  test: { //se debe agregar??
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres'
-  },
   production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
